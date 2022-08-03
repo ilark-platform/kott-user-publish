4f033daff21859f5aa7aae039084909d84dc3e4e
rnjsdhrb84 <59335661+rnjsdhrb84@users.noreply.github.com>
2022-08-03 16:56:51 +0900

    Add files via upload

---
+++ html/assets/js/function.js
@@ -0,0 +1,120 @@
+"use strict";
+
+let sbartrigger = document.querySelector('.js-sidebar-trigger');
+let sbarclose = document.querySelector('.js-sidebar-close');
+let sidebar = document.querySelector('.sidebar');
+let dimlayer = document.querySelector('.dim-layer');
+
+if(sidebar){
+  let classOpen = [sidebar, dimlayer];
+
+  sbartrigger.addEventListener('click', function(e){
+    classOpen.forEach(e => e.classList.add('is-active'));
+  });
+
+  let classCloseClick = [dimlayer, sbarclose];
+  classCloseClick.forEach(function(el) {
+    el.addEventListener('click', function(els) {
+      classOpen.forEach(els => els.classList.remove('is-active'));
+    });
+  });
+}
+
+/*===== SIDEBAR & HEADER COLLAPSE MENU  =====*/
+let linkCollapse = document.getElementsByClassName('collapse__link');
+for(var i=0; i<linkCollapse.length; i++){
+  linkCollapse[i].addEventListener('click', function(){
+    let collapseMenu = this.nextElementSibling
+    collapseMenu.classList.toggle('collapse__list--open')
+
+    let previousEl = collapseMenu.previousElementSibling
+    previousEl.classList.toggle('global-nav__link--open')
+  })
+}
+
+
+/* 모달 테스트용 */
+let modal = document.getElementById('modal');
+function openModal() {
+    modal.style.display = "block";
+}
+function closeModal() {
+  modal.style.display = "none";  
+}
+
+/* 펼침 */
+let elToggle = document.getElementsByClassName('js-showhide');
+
+for(var i=0; i<elToggle.length; i++){
+  elToggle[i].nextElementSibling.style.display = "none";
+
+  elToggle[i].addEventListener('click', function(){
+    let targetEl = this.nextElementSibling
+    if (targetEl.style.display=='block') {
+      targetEl.style.display='none';
+    }
+    else {
+      targetEl.style.display='block';
+    }
+    
+    let previousEl = targetEl.previousElementSibling
+    previousEl.classList.toggle('is-open')
+    
+  })
+}
+
+
+/* 커스텀 셀렉트 */
+const label = document.querySelectorAll('.select-dropdown__label');
+label.forEach(function(lb){
+    lb.addEventListener('click', e => {
+        let optionList = lb.nextElementSibling;
+        let optionItems = optionList.querySelectorAll('.select-dropdown__item');
+        clickLabel(lb, optionItems);
+    })
+});
+const clickLabel = (lb, optionItems) => {
+    if(lb.parentNode.classList.contains('expanded')) {
+        lb.parentNode.classList.remove('expanded');
+        optionItems.forEach((opt) => {
+            opt.removeEventListener('click', () => {
+                handleSelect(lb, opt)
+            })
+        })
+    } else {
+        lb.parentNode.classList.add('expanded');
+        optionItems.forEach((opt) => {
+            opt.addEventListener('click', () => {
+                handleSelect(lb, opt)
+            })
+        })
+    }
+}
+const handleSelect = (label, item) => {
+    label.innerHTML = item.textContent;
+    label.parentNode.classList.remove('expanded');
+}
+
+
+/* 비밀번호 Show & Hide */
+var parentForm = document.querySelector(".form");
+
+function pwShow(input, showText) {
+  if (input.getAttribute("type") === "password") {
+    input.setAttribute("type", "text");
+    showText.classList.add('expanded');
+  } else {
+    input.setAttribute("type", "password");
+    showText.classList.remove('expanded');
+  }
+}
+
+if(parentForm){
+  parentForm.addEventListener("click", event => {
+    if (event.target.matches('.js-pwToggle')) {
+      var btnElm = event.target;
+      var inputElm = btnElm.previousElementSibling;
+      pwShow(inputElm, btnElm);
+    }
+  });
+}
